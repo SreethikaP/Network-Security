164ff7af5a3c01a0e6869b4ee24e9d8f11a7b964
Sreethika Padala <padalasreethika@gmail.com>
2025-04-09 20:51:27 -0500

    changes done

--- app.py
+++ app.py
@@ -99,5 +99,5 @@ async def predict_route(request: Request,file: UploadFile = File(...)):
             raise NetworkSecurityException(e,sys)
 
     
-if _name=="main_":
+if __name__=="__main_":
     app_run(app,host="0.0.0.0",port=8000)
